bb4d96cdbdf3e831c019d43f4c0da54b1b37ce52
Mariana Oliveira <olvr.mariana@gmail.com>
2019-07-15 00:49:19 -0300

    tira as chaves da string de conexão

--- reclamacoesRepository.js
+++ reclamacoesRepository.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URL = 'mongodb://<marianaolvr>:<Olvrsaber13mlab>@ds133137.mlab.com:33137/heroku_77343qfl'
+const MONGO_URL = 'mongodb://marianaolvr:Olvrsaber13mlab@ds133137.mlab.com:33137/heroku_77343qfl'
 
 function connect (){
     mongoose.connect(MONGO_URL, {useNewUrlParser: true},
